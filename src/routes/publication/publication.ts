c7a0b8fc1d71645b1958b4f05f1ce047c22e11d3
Rolo365 <jmoralesc2005@alumno.ipn.mx>
2023-03-25 12:48:52 -0600

    publication unfind id handler

--- src/routes/publication/publication.ts
+++ src/routes/publication/publication.ts
@@ -3,9 +3,10 @@ import { PublicationImage } from '../../classes/publication/image.js';
 import { ComentDB } from '../../database/publication/coment.js';
 import { ImageDB } from '../../database/publication/image.js';
 import { PublicationDB } from '../../database/publication/publication.js';
+import { PublicationTypeDB } from '../../database/publication/publicationType.js';
 import { ReactionDB } from '../../database/publication/reaction.js';
+import { SettlementDB } from '../../database/ubication/settlement.js';
 import { UserDB } from '../../database/user/user.js';
-import { returnImage } from '../../helpers/publication/image.js';
 import { returnPublication, returnPublicationJSON } from '../../helpers/publication/publication.js';
 import { middleware } from '../../middleware/index.js';
 
@@ -15,6 +16,8 @@ const userbd = new UserDB();
 const reactionbd = new ReactionDB();
 const comentbd = new ComentDB();
 const imagebd = new ImageDB();
+const settlementbd = new SettlementDB();
+const publicationTypebd = new PublicationTypeDB();
 
 publicationRoutes.get('/get-publications', middleware, async (req, res) => {
 
@@ -53,23 +56,107 @@ publicationRoutes.post('/create-publication', middleware, async (req, res) => {
 
     if (typeof body.description == 'string' && body.date instanceof Date && typeof body.id_publication_type == 'number' && typeof body.id_user == 'number' && typeof body.id_settlement == 'number') {
 
-      if (body?.images?.length > 0) {
+      let response = {
+        ok: true,
+        error: ""
+      }
 
-        let error_1 = false;
+      const user = await userbd.getUser(body.id_user)
+      const settlement = await settlementbd.getSettlement(body.id_settlement);
+      const publicationType = await publicationTypebd.getPublicationType(body.id_publication_type)
 
-        body?.images?.forEach((image: any) => {
-          if (!error_1) {
-            if (!image.url) {
-              error_1 = true;
+      if (!user?.getIdUser()) {
+        response.error = "User does not exist"
+        response.ok = false
+      } else if (!settlement?.getIdSettlement()) {
+        response.error = "Settlement does not exist"
+        response.ok = false
+      } else if (!publicationType?.getIdPublicationType()) {
+        response.error = "Publication type does not exist"
+        response.ok = false
+      }
+
+      if (response.ok) {
+        if (body?.images?.length > 0) {
+
+          let error_1 = false;
+
+          body?.images?.forEach((image: any) => {
+            if (!error_1) {
+              if (!image.url) {
+                error_1 = true;
+              }
             }
-          }
-        })
+          })
 
-        if (error_1) {
-          return res.status(200).send({
-            ok: false,
-            error_1: "Invalid Image URL"
-          });
+          if (error_1) {
+            return res.status(200).send({
+              ok: false,
+              error_1: "Invalid Image URL"
+            });
+          } else {
+
+            const last_publication = (await publicationbd.getLastPublication())[0];
+
+            body.id_publication = <number>last_publication.getIdPublication() + 1;
+
+            const publication = returnPublication(body);
+
+            const publication_saved = await publicationbd.createPublication(publication);
+
+            let error_2 = false;
+
+            if (publication_saved) {
+
+              for (let i = 0; i < body?.images?.length; i++) {
+
+                if (!error_2) {
+                  const last_image = (await imagebd.getLastImage())[0]
+                  const id_image = <number>last_image.getIdImage() + i + 1;
+
+                  const image = new PublicationImage(id_image, <string>body?.images[i]?.url, false)
+
+                  const saved_image = await imagebd.saveImage(image, publication);
+
+                  if (!saved_image) {
+                    error_2 = true;
+                    return res.status(200).send({
+                      ok: false,
+                      error: "Data Base Error"
+                    });
+                  }
+                }
+
+              }
+
+              if (!error_2) {
+                const response_publication = await publicationbd.getPublication(publication.getIdPublication());
+
+                const user = await userbd.getUser(response_publication?.getUser()?.getIdUser())
+                const reactions = await reactionbd.getReactionsPerPublication(response_publication?.getIdPublication())
+                const coments = await comentbd.getComentsPerPublication(response_publication?.getIdPublication())
+                const images = await imagebd.getImagesPerPublication(response_publication?.getIdPublication())
+
+                response_publication?.setUser(user)
+                response_publication?.setReactions(reactions)
+                response_publication?.setComents(coments)
+                response_publication?.setImages(images)
+
+                return res.status(200).send({
+                  ok: true,
+                  publication: returnPublicationJSON(response_publication)
+                });
+              }
+
+            } else {
+              return res.status(200).send({
+                ok: false,
+                error: "Data Base Error"
+              });
+            }
+
+          }
+
         } else {
 
           const last_publication = (await publicationbd.getLastPublication())[0];
@@ -80,49 +167,23 @@ publicationRoutes.post('/create-publication', middleware, async (req, res) => {
 
           const publication_saved = await publicationbd.createPublication(publication);
 
-          let error_2 = false;
-
           if (publication_saved) {
+            const response_publication = await publicationbd.getPublication(publication.getIdPublication());
 
-            for (let i = 0; i < body?.images?.length; i++) {
+            const user = await userbd.getUser(response_publication?.getUser()?.getIdUser())
+            const reactions = await reactionbd.getReactionsPerPublication(response_publication?.getIdPublication())
+            const coments = await comentbd.getComentsPerPublication(response_publication?.getIdPublication())
+            const images = await imagebd.getImagesPerPublication(response_publication?.getIdPublication())
 
-              if (!error_2) {
-                const last_image = (await imagebd.getLastImage())[0]
-                const id_image = <number>last_image.getIdImage() + i + 1;
+            response_publication?.setUser(user)
+            response_publication?.setReactions(reactions)
+            response_publication?.setComents(coments)
+            response_publication?.setImages(images)
 
-                const image = new PublicationImage(id_image, <string>body?.images[i]?.url, false)
-
-                const saved_image = await imagebd.saveImage(image, publication);
-
-                if (!saved_image) {
-                  error_2 = true;
-                  return res.status(200).send({
-                    ok: false,
-                    error: "Data Base Error"
-                  });
-                }
-              }
-
-            }
-
-            if (!error_2) {
-              const response_publication = await publicationbd.getPublication(publication.getIdPublication());
-
-              const user = await userbd.getUser(response_publication?.getUser()?.getIdUser())
-              const reactions = await reactionbd.getReactionsPerPublication(response_publication?.getIdPublication())
-              const coments = await comentbd.getComentsPerPublication(response_publication?.getIdPublication())
-              const images = await imagebd.getImagesPerPublication(response_publication?.getIdPublication())
-
-              response_publication?.setUser(user)
-              response_publication?.setReactions(reactions)
-              response_publication?.setComents(coments)
-              response_publication?.setImages(images)
-
-              return res.status(200).send({
-                ok: true,
-                publication: returnPublicationJSON(response_publication)
-              });
-            }
+            return res.status(200).send({
+              ok: true,
+              publication: returnPublicationJSON(response_publication)
+            });
 
           } else {
             return res.status(200).send({
@@ -132,42 +193,11 @@ publicationRoutes.post('/create-publication', middleware, async (req, res) => {
           }
 
         }
-
       } else {
-
-        const last_publication = (await publicationbd.getLastPublication())[0];
-
-        body.id_publication = <number>last_publication.getIdPublication() + 1;
-
-        const publication = returnPublication(body);
-
-        const publication_saved = await publicationbd.createPublication(publication);
-
-        if (publication_saved) {
-          const response_publication = await publicationbd.getPublication(publication.getIdPublication());
-
-          const user = await userbd.getUser(response_publication?.getUser()?.getIdUser())
-          const reactions = await reactionbd.getReactionsPerPublication(response_publication?.getIdPublication())
-          const coments = await comentbd.getComentsPerPublication(response_publication?.getIdPublication())
-          const images = await imagebd.getImagesPerPublication(response_publication?.getIdPublication())
-
-          response_publication?.setUser(user)
-          response_publication?.setReactions(reactions)
-          response_publication?.setComents(coments)
-          response_publication?.setImages(images)
-
-          return res.status(200).send({
-            ok: true,
-            publication: returnPublicationJSON(response_publication)
-          });
-
-        } else {
-          return res.status(200).send({
-            ok: false,
-            error: "Data Base Error"
-          });
-        }
-
+        return res.status(200).send({
+          ok: false,
+          error: response.error
+        });
       }
 
     } else {
