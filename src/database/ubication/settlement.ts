89940eac9930f6f2b7a7f6bafaf8f9e354dd0a15
Rolo365 <jmoralesc2005@alumno.ipn.mx>
2023-03-25 12:40:42 -0600

    get settlement id

--- src/database/ubication/settlement.ts
+++ src/database/ubication/settlement.ts
@@ -24,9 +24,34 @@ class SettlementDB {
           console.error(error);
         } else {
           if (result) {
-            let postalCodes: Settlement[] = result.map((data: any) => returnSettlement(data));
+            let settlements: Settlement[] = result.map((data: any) => returnSettlement(data));
 
-            resolve(postalCodes);
+            resolve(settlements);
+          };
+        };
+      });
+    });
+    return promise;
+  }
+
+  getSettlement = (id: number) => {
+    const promise = new Promise<Settlement>((resolve) => {
+      this.#con.query(`
+        SELECT * FROM msettlement AS stl
+        INNER JOIN cpostalcode AS pc ON stl.id_zip_pc = pc.zip_pc
+        INNER JOIN cstate AS st ON pc.id_state = st.id_state
+        INNER JOIN cmunicipality AS mu ON pc.id_municipality = mu.id_municipality
+        INNER JOIN csettlementtype AS stl_type ON stl.id_settlement_type = stl_type.id_settlement_type
+        WHERE stl.id_settlement = ${id}
+        ORDER BY 'id_settlement' DESC
+      ;`, (error: any, result: any) => {
+        if (error) {
+          console.error(error);
+        } else {
+          if (result) {
+            let settlement: Settlement = result.map((data: any) => returnSettlement(data))[0];
+
+            resolve(settlement);
           };
         };
       });
