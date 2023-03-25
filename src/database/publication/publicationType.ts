88f59c55bb3c84fde6785cd9ce14a2f016a8cbfc
Rolo365 <jmoralesc2005@alumno.ipn.mx>
2023-03-25 12:47:32 -0600

    get publication type id

--- src/database/publication/publicationType.ts
+++ src/database/publication/publicationType.ts
@@ -14,13 +14,34 @@ class PublicationTypeDB {
     const promise = new Promise<PublicationType[]>((resolve) => {
       this.#con.query(`
         SELECT * FROM cpublicationtype
-        ORDER BY 'id_publication_type' DESC
+        ORDER BY \`id_publication_type\` DESC
       ;`, (error: any, result: any) => {
         if (error) {
           console.error(error);
         } else {
           if (result) {
             let publicationTypes: PublicationType[] = result.map((data: any) => returnPublicationType(data));
+
+            resolve(publicationTypes);
+          };
+        };
+      });
+    });
+    return promise;
+  }
+
+  getPublicationType = (id: number) => {
+    const promise = new Promise<PublicationType>((resolve) => {
+      this.#con.query(`
+        SELECT * FROM cpublicationtype
+        WHERE \`id_publication_type\` = ${id}
+        ORDER BY \`id_publication_type\` DESC
+      ;`, (error: any, result: any) => {
+        if (error) {
+          console.error(error);
+        } else {
+          if (result) {
+            let publicationTypes: PublicationType = result.map((data: any) => returnPublicationType(data))[0];
 
             resolve(publicationTypes);
           };
