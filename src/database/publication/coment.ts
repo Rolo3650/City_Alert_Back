564b5c86438a7564283dab7bd6c21b2cbf2cb288
Rolo365 <jmoralesc2005@alumno.ipn.mx>
2023-03-25 12:29:05 -0600

    save coment

--- src/database/publication/coment.ts
+++ src/database/publication/coment.ts
@@ -1,4 +1,6 @@
+import moment from "moment";
 import { Coment } from "../../classes/publication/coment.js";
+import { Publication } from "../../classes/publication/publication.js";
 import { Municipality } from "../../classes/ubication/municipality.js";
 import { PostalCode } from "../../classes/ubication/postalCode.js";
 import { Settlement } from "../../classes/ubication/settlement.js";
@@ -71,6 +73,72 @@ class ComentDB {
             let coments: Coment[] = result.map((data: any) => returnComent(data));
 
             resolve(coments);
+          };
+        };
+      });
+    });
+    return promise;
+  }
+
+  getLastComent = () => {
+    const promise = new Promise<Coment[]>((resolve) => {
+      this.#con.query(`
+      SELECT * FROM mcoment AS cm
+      INNER JOIN muser AS us ON cm.id_user = us.id_user
+      INNER JOIN cusertype AS cu ON us.id_user_type = cu.id_user_type
+      INNER JOIN mperson AS pe ON us.id_person = pe.id_person
+      INNER JOIN csex AS sx ON pe.id_sex = sx.id_sex
+      INNER JOIN msettlement AS stl ON pe.id_settlement = stl.id_settlement
+      INNER JOIN cpostalcode AS pc ON stl.id_zip_pc = pc.zip_pc
+      INNER JOIN cstate AS st ON pc.id_state = st.id_state
+      INNER JOIN cmunicipality AS mu ON pc.id_municipality = mu.id_municipality
+      INNER JOIN csettlementtype AS stl_type ON stl.id_settlement_type = stl_type.id_settlement_type
+      ORDER BY \`id_coment\` DESC
+      LIMIT 1
+      ;`, (error: any, result: any) => {
+        if (error) {
+          console.error(error);
+        } else {
+          if (result) {
+            let coments: Coment[] = result.map((data: any) => returnComent(data));
+
+            resolve(coments);
+          };
+        };
+      });
+    });
+    return promise;
+  }
+
+  saveComent = (coment: Coment | null | undefined, publication: Publication | null | undefined) => {
+    const promise = new Promise<boolean>((resolve) => {
+      this.#con.query(`
+        INSERT INTO mcoment (
+        \`id_coment\`,
+        \`coment\`,
+        \`date\`,
+        \`deleted\`,
+        \`id_publication\`,
+        \`id_user\`)
+        VALUES (
+        '${coment?.getIdComent()}',
+        '${coment?.getComent()}',
+        '${moment(coment  ?.getDate()).format("YYYY-MM-DD hh:mm:ss")}',
+        '0',
+        '${coment?.getUser()?.getIdUser()}',
+        '${publication?.getIdPublication()}')
+      ;`, (error: any, result: any) => {
+        if (error) {
+          console.error(error);
+        } else {
+          if (result) {
+            if (result.serverStatus == 2) {
+              resolve(true);
+            } else {
+              resolve(false);
+            }
+          } else {
+            resolve(false);
           };
         };
       });
--- src/routes/publication/coment.ts
+++ src/routes/publication/coment.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import { Publication } from '../../classes/publication/publication.js';
 import { ComentDB } from '../../database/publication/coment.js';
-import { returnComentJSON } from '../../helpers/publication/coment.js';
+import { PublicationDB } from '../../database/publication/publication.js';
+import { UserDB } from '../../database/user/user.js';
+import { returnComent, returnComentJSON } from '../../helpers/publication/coment.js';
 import { middleware } from '../../middleware/index.js';
 
 const comentRoutes = express();
 const comentbd = new ComentDB();
+const userdb = new UserDB();
+const publicationdb = new PublicationDB();
 
 comentRoutes.get('/get-coments', middleware, async (req, res) => {
 
@@ -20,14 +25,84 @@ comentRoutes.get('/get-coments', middleware, async (req, res) => {
 
 comentRoutes.post('/create-coment', middleware, async (req, res) => {
 
-  let { body } = req
+  let { body } = req;
 
   body.date = new Date()
 
-  return res.status(200).send({
-    ok: true,
-    body
-  });
+  if (body.coment && body.id_publication && body.id_user) {
+
+    if (typeof body.coment == 'string' && typeof body.id_publication == 'number' && typeof body.id_user == 'number' && body.date instanceof Date) {
+
+      let response = {
+        ok: true,
+        error: ""
+      }
+
+      const user = await userdb.getUser(body.id_user)
+      const publication = await publicationdb.getPublication(body.id_publication)
+
+      if (!user?.getIdUser()) {
+        response.error = "User does not exist"
+        response.ok = false
+      } else if (!publication?.getIdPublication()) {
+        response.error = "Publication does not exist"
+        response.ok = false
+      }
+
+      if (!response.ok) {
+        return res.status(200).send({
+          ok: false,
+          error: response.error
+        });
+      } else {
+
+        const last_coment = (await comentbd.getLastComent())[0];
+
+        const id_coment = <number> last_coment.getIdComent() + 1;
+
+        body.id_coment = id_coment;
+
+        const coment = returnComent(body);
+        const publication = new Publication(body.id_publication, null, null, null, null, null, null, null, null, null, null)
+
+        // console.log("date", coment.getDate())
+
+        const coment_saved = await comentbd.saveComent(coment, publication);
+
+        if (coment_saved) {
+
+          return res.status(200).send({
+            ok: true,
+            body
+          });
+
+        } else {
+
+          return res.status(200).send({
+            ok: false,
+            error: "Data base error"
+          });
+
+        }
+      }
+
+    } else {
+
+      return res.status(200).send({
+        ok: false,
+        error: "Invalid Data"
+      });
+
+    }
+
+  } else {
+
+    return res.status(200).send({
+      ok: false,
+      error: "Missing Data"
+    });
+
+  }
 
 });
 
